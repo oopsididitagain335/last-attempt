9b179282633f0b5488b43bcab3d22a06e69b951c
Solace <monkdev9@gmail.com>
2025-08-28 23:12:01 +0100

    Update db.js

--- pages/api/utils/db.js
+++ pages/api/utils/db.js
@@ -1,1 +1,16 @@
+let client;
+let clientPromise;
 
+if (!global._mongoClientPromise) {
+  const { MongoClient } = require('mongodb');
+  client = new MongoClient(process.env.MONGO_URI);
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+export async function getDb() {
+  const client = await clientPromise;
+  return client.db('thebiolink');
+}
+
+export default clientPromise;

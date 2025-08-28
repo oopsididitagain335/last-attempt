f61288ac81c4771153507bb1137bc6223e160fc3
Solace <monkdev9@gmail.com>
2025-08-28 22:58:36 +0100

    Update discord-login.js

--- pages/api/auth/discord-login.js
+++ pages/api/auth/discord-login.js
@@ -1,9 +1,13 @@
 // pages/api/auth/discord-login.js
 
+/**
+ * Redirects user to Discord OAuth2 login page
+ */
+
 export default function handler(req, res) {
   const clientId = process.env.DISCORD_CLIENT_ID;
   const redirectUri = encodeURIComponent(`${process.env.BASE_URL}/api/auth/discord-callback`);
-  const scope = encodeURIComponent('identify email guilds');
+  const scope = encodeURIComponent('identify email');
   const discordOauthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}&prompt=consent`;
 
   res.redirect(discordOauthUrl);
